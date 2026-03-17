b80248e8665f9e97235d6062b6c7f8d12b4dd06f
sumanchapagain50 <144764239+sumanchapagain50@users.noreply.github.com>
2026-03-17 09:38:29 +0545

    Update activities.js

--- activities.js
+++ activities.js
@@ -2,31 +2,31 @@ const activities = [
     {
         id: "act_1",
         name: "Health Post Rehabilitation",
-        indicatorIds: ["h1", "p1"],
-        communityIds: ["comm_1", "comm_2"],
+        indicatorIds: ["H01", "F01"],
+        communityIds: ["c_01", "c_01", "c_04", "c_03"],
         knowledgeGenerated: true,
         beneficiaries: { men: 120, women: 150, oldMen: 30, oldWomen: 45, newMen: 90, newWomen: 105 }
     },
     {
         id: "act_2",
         name: "Disaster Risk Training",
-        indicatorIds: ["h2", "s1", "s2"],
-        communityIds: ["comm_1"],
+        indicatorIds: ["H02", "S01", "S02"],
+        communityIds: ["c_11"],
         knowledgeGenerated: false,
         beneficiaries: { men: 50, women: 60, oldMen: 10, oldWomen: 15, newMen: 40, newWomen: 45 }
     },
     {
         id: "act_3",
         name: "Mangrove Reforestation",
-        indicatorIds: ["n1", "n2"],
-        communityIds: ["comm_2"],
+        indicatorIds: ["N01", "N02"],
+        communityIds: ["c_22", "c_31"],
         knowledgeGenerated: true,
         beneficiaries: { men: 200, women: 180, oldMen: 50, oldWomen: 40, newMen: 150, newWomen: 140 }
     },
     {
         id: "act_4",
         name: "Community Micro-finance Setup",
-        indicatorIds: ["f1", "f2", "s1"],
+        indicatorIds: ["F01", "F02", "S01"],
         communityIds: ["comm_1", "comm_2"],
         knowledgeGenerated: false,
         beneficiaries: { men: 40, women: 300, oldMen: 10, oldWomen: 50, newMen: 30, newWomen: 250 }
